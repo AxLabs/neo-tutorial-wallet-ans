02cf1505a3b1422d1d58b8c3b7aed71319d81df2
Guilherme Sperb Machado <gsm@machados.org>
2020-04-11 11:55:21 +0200

    serve static content from spring boot + CORS

---
+++ src/main/java/neo/tutorial/wallet/ans/StaticContentConfig.java
@@ -0,0 +1,34 @@
+package neo.tutorial.wallet.ans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticContentConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/dapp/**")
+                .addResourceLocations("classpath:/dapp-frontend/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/dapp", "/dapp/index.html");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
+}
